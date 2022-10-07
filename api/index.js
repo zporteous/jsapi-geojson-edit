13801cd1572076144a0b3950cc126e860b5ef601
zach <porteous.zj@gmail.com>
2022-10-07 15:45:03 +0000

    node serving static

--- api/index.js
+++ api/index.js
@@ -3,26 +3,18 @@ cookieParser = require("cookie-parser");
 const pg = require('pg');
 const path = require('path');
 
-const cors = require("cors");
-const oneDay = 1000 * 60 * 60 * 24;
-
 var app = express();
 app.use(express.json({limit:'20mb'}));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-  origin:`http://localhost:3000`,
-  origin:`*`,
-  credentials:true,
-  methods: ['POST', 'PUT', 'GET'],
-}));
+
 
 const mountRoutes = require('./routes')
 mountRoutes(app)
 
-app.use(express.static(path.join(__dirname,'client')));
+app.use(express.static(path.join('client','static')));
 
-app.get('/', function (req, res) {
+app.get('/jsapi-geojson-edit', function (req, res) {
   res.sendFile(path.join(__dirname,'client', 'index.html'));
 });
 
